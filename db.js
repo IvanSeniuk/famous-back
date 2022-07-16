a877e67a6bce3c603a82d3eae15bc2d8083505a0
ivanseniuk <iv.seniuk@gmail.com>
2022-07-16 13:49:27 +0300

    postges edit

--- db.js
+++ db.js
@@ -1,13 +1,21 @@
 const { Sequelize } = require("sequelize");
 
+//module.exports = new Sequelize(
+//	process.env.DB_NAME,
+//	process.env.DB_USER,
+//	process.env.DB_PASSWORD,
+//	{
+//		dialect: "postgres",
+//		host: process.env.DB_HOST,
+//		port: process.env.DB_PORT,
+//	}
+//);
 module.exports = new Sequelize(
-	process.env.DB_NAME,
-	process.env.DB_USER,
-	process.env.DB_PASSWORD,
+	"postgres://postgres:postgres@localhost/famous_db",
 	{
 		dialect: "postgres",
-		host: process.env.DB_HOST,
-		port: process.env.DB_PORT,
+		//host: process.env.DB_HOST,
+		//port: process.env.DB_PORT,
 	}
 );
 
