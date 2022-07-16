ea1929dc6cefdc9de87df72430a3a6ffe9d73b73
ivanseniuk <iv.seniuk@gmail.com>
2022-07-16 14:45:22 +0300

    postges edit

--- db.js
+++ db.js
@@ -1,20 +1,20 @@
 const { Sequelize } = require("sequelize");
 
-//module.exports = new Sequelize(
-//	process.env.DB_NAME,
-//	process.env.DB_USER,
-//	process.env.DB_PASSWORD,
-//	{
-//		dialect: "postgres",
-//		host: process.env.DB_HOST,
-//		port: process.env.DB_PORT,
-//	}
-//);
-module.exports = new Sequelize("famous_db", "postgres", "root", {
-	dialect: "postgres",
-	host: "localhost",
-	port: 5432,
-});
+module.exports = new Sequelize(
+	process.env.DB_NAME,
+	process.env.DB_USER,
+	process.env.DB_PASSWORD,
+	{
+		dialect: "postgres",
+		host: process.env.DB_HOST,
+		port: process.env.DB_PORT,
+	}
+);
+//module.exports = new Sequelize("famous_db", "postgres", "root", {
+//	dialect: "postgres",
+//	host: "localhost",
+//	port: 5432,
+//});
 //DB_NAME=
 //DB_USER=postgres
 //DB_PASSWORD=root
