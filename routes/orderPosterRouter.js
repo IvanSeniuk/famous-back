b3aa4ae308ba6678e8933f5575bfdc57aee6eb5d
ivanseniuk <iv.seniuk@gmail.com>
2022-07-20 11:39:25 +0300

    change OrderPosterRouter

--- routes/orderPosterRouter.js
+++ routes/orderPosterRouter.js
@@ -1,11 +1,11 @@
 const Router = require("express");
 const router = new Router();
 const orderPosterController = require("../controllers/orderPosterController");
-const checkRole = require("../middlewere/checkRoleMiddlewere");
+//const checkRole = require("../middlewere/checkRoleMiddlewere");
 
 router.post("/", orderPosterController.create);
-router.get("/", checkRole("ADMIN" || "USER"), orderPosterController.getAll);
-router.get("/:id", checkRole("ADMIN" || "USER"), orderPosterController.getOne);
+router.get("/", orderPosterController.getAll);
+router.get("/:id", orderPosterController.getOne);
 //router.put("/:id", checkRole("ADMIN"), orderPosterController.update);
 //router.delete("/:id", checkRole("ADMIN"), orderPosterController.delete);
 
